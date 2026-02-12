a742e196a5d8fa3cfe5a907fc8addf1b67d5137e
Mehedi Hasan <mehedi.hasan11023@gmail.com>
2026-02-12 23:23:31 +0600

    practise js

---
+++ day1.js
@@ -0,0 +1,31 @@
+const name = 'Bangladesh';
+
+console.log(name)
+
+
+const countryName = 'BD' + name ;
+
+console.log(countryName)
+
+let price = 120;
+price = 150;
+console.log(price)
+
+
+console.log( 'device value',device)
+var device ='laptop'
+
+
+
+const charge = 50;
+
+const dress = ['shirt','pant','jacket'];
+
+dress.push = ('maflar');
+
+console.log(dress)
+
+
+const student = {name: 'hasan' , marks: 50 }
+student.marks = 60 ;
+console.log(student)
